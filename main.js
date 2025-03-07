7b47bf898ff1520656c69b62b8fc69ca3feb4631
Farah ABDOURAZAK <farah.abdourazak@unilasalle.fr>
2025-03-07 14:23:18 +0100

    Amélioration du CSS pour occuper toute la page

--- main.js
+++ main.js
@@ -1,122 +1,122 @@
-// parametrage de la carte coordonnees et le zoom
-var map= L.map('map').setView([49.44189804185607, 1.0934024503415607],10);
+// Initialisation de la carte avec coordonnées et zoom
+let map = L.map('map').setView([49.4419, 1.0934], 10);
 
-// etape 3 : Ajouter un fond du carte 
-// liens pour telecharge les tuiles : Leaflet Provider Demo (leaflet-extras.github.io)
-var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+// Ajouter le fond de carte par défaut
+let osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+let OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org">OpenStreetMap</a> contributors'
+});
 
-var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-	maxZoom: 17,
-	attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-}).addTo(map);
+let Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+    maxZoom: 20,
+    attribution: '&copy; <a href="https://www.stadiamaps.com/">Stadia Maps</a>'
+});
 
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-}).addTo(map);
-
-// parametre les fonds cartes
-var basmap= {
-    'Open Street Map' : osm,
+// Contrôle pour changer le fond de carte
+let basemap = {
+    'Open Street Map': osm,
     'Open Topo Map': OpenTopoMap,
-    'Stadia AlidadeSmoothDark':Stadia_AlidadeSmoothDark
+    'Stadia Dark': Stadia_AlidadeSmoothDark
 };
 
-L.control.layers(basmap).addTo(map)
+L.control.layers(basemap).addTo(map);
+L.control.scale().addTo(map); // Ajout de l'échelle
 
-// connecter le fiche js avec le donnée geojson 
-//var rouen = L.geoJSON(rouen_test).addTo(map)
-//fonction qui affecte une coleur à la densite de population 
-function getColor(densite){
-    return densite > 5413 ? '#641e16':
-           densite > 1500 ? '#a93226':
-           densite > 300 ? '#cd6155':
-           densite > 25 ? '#d98880':
+// Fonction pour attribuer une couleur en fonction de la densité
+function getColor(densite) {
+    return densite > 5413 ? '#641e16' :
+           densite > 1500 ? '#a93226' :
+           densite > 300 ? '#cd6155' :
+           densite > 25 ? '#d98880' :
                           '#feedde';
 }
 
-function style(features){
+// Style des zones GeoJSON
+function style(feature) {
     return {
-        fillColor : getColor(features.properties.densite),
-        weight : 1,
-        opacity:1,
-        color:'white',
+        fillColor: getColor(feature.properties.densite),
+        weight: 1,
+        opacity: 1,
+        color: 'white',
         dashArray: '3',
-        fillOpacity: 0.95
+        fillOpacity: 0.9
     };
 }
 
-//L.geoJSON(rouen_f,{style:style}).addTo(map)
-
-//fonction pour ajouter le contour 
-function HighlightFeature(e){
-    var layer= e.target;
+// Mettre en surbrillance une zone
+function highlightFeature(e) {
+    let layer = e.target;
     layer.setStyle({
-        weight:2,
+        weight: 2,
         color: 'red',
-        dashArray:'1',
-        fillOpacity:0.8
+        dashArray: '1',
+        fillOpacity: 0.8
     });
     layer.bringToFront();
 }
 
-//fonction pour réintialiser le contour 
-function resetHighlight(e){
+// Réinitialiser le style d'origine
+function resetHighlight(e) {
     geojson.resetStyle(e.target);
 }
 
-//fonction qui lie les événements
-function onEachFeature(feature,layer){
-    var popupContent ="<strong>"+ feature.properties.nom+"<strong><br>"+ "Densité: " + feature.properties.densite+"habitants/km²";
-
-    layer.on({
-        mouseover:function(e){
-            HighlightFeature(e);   //surline lors du sourvol
-            layer.openPopup();     //Ouvrir le popup lors du survol
-        },
-        mouseout:function(e){
-            resetHighlight(e);     //effacer le surliner lors du sourvol
-            layer.closePopup();    //ferme le popup lors du survol
-        },
-        click: zoomToFeature,      //
-    });
-    layer.bindPopup(popupContent); //
-}
-function zoomToFeature(e){
+// Fonction de zoom sur une zone
+function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
 }
 
-var geojson = L.geoJSON(rouen_f,{style:style, onEachFeature:onEachFeature}).addTo(map)
+// Associer événements et popups
+function onEachFeature(feature, layer) {
+    let popupContent = `<strong>${feature.properties.nom}</strong><br> Densité: ${feature.properties.densite} hab/km²`;
 
-//ajouter une legend
+    layer.on({
+        mouseover: function (e) {
+            highlightFeature(e);
+            layer.openPopup();
+        },
+        mouseout: function (e) {
+            resetHighlight(e);
+            layer.closePopup();
+        },
+        click: zoomToFeature
+    });
 
-var legend= L.control({position:'bottomleft'});
+    layer.bindPopup(popupContent);
+}
 
-legend.onAdd=function(map){
-    var div = L.DomUtil.create('div','info legend'),
+// Vérifier si les données GeoJSON sont définies
+if (typeof rouen_f !== 'undefined') {
+    var geojson = L.geoJSON(rouen_f, {
+        style: style,
+        onEachFeature: onEachFeature
+    }).addTo(map);
+}
 
-        grades=[0, 25, 300, 1500, 5413],
-        labels=[],
-        from,to;
-        div.innerHTML='<p>Densité de la population (hab/km²) </p>';
-    
-        for(var i=0;i<grades.length;i++){
-        from=grades[i];
-        to=grades[i+1];
+// Ajouter une légende
+let legend = L.control({ position: 'bottomleft' });
+
+legend.onAdd = function (map) {
+    let div = L.DomUtil.create('div', 'info legend');
+    let grades = [0, 25, 300, 1500, 5413];
+    let labels = [];
+
+    div.innerHTML = '<p><strong>Densité de la population (hab/km²)</strong></p>';
+
+    for (let i = 0; i < grades.length; i++) {
+        let from = grades[i];
+        let to = grades[i + 1];
 
         labels.push(
-            '<i style="background:' +getColor(from + 1)+ '"></i>'+
-            from+ (to ? '&ndash;' + to :'+'));
+            `<i style="background:${getColor(from + 1)}"></i> ${from}${to ? '&ndash;' + to : '+'}`
+        );
     }
-    
-    div.innerHTML+=labels.join('<br>');
-    
+
+    div.innerHTML += labels.join('<br>');
     return div;
 };
 
-legend.addTo(map)
+legend.addTo(map);
